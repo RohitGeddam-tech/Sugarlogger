5d54952edb91721e4ca420c8eff1f6f029989976
RohitGeddam-tech <geddamrohi13@gmail.com>
2021-04-14 18:27:09 +0530

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,17 +6,17 @@
 
 // You can delete this file if you're not using it
 
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-//     if (stage === "build-html" || stage === "develop-html") {
-//       actions.setWebpackConfig({
-//         module: {
-//           rules: [
-//             {
-//               test: /src/,
-//               use: [loaders.null()]
-//             },
-//           ],
-//         },
-//       })
-//     }
-//   }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /src/components/MainFooter,
+            use: [loaders.null()],
+          },
+        ],
+      },
+    })
+  }
+}
