77785babb457b26412ea276bcd77d6d2c8cbc972
RohitGeddam-tech <RohitGeddam-tech@users.noreply.github.com>
2021-05-26 14:57:05 +0000

    deployed via Gatsby Publish Action 🎩 for 005c4ba7b2dcf4c16c38e7b411b434beba51c661

---
+++ component---src-pages-article-js-d0371c419f683f7e4d04.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[197],{3531:function(e,t,a){"use strict";a.r(t);var l=a(7294),u=a(5364),n=a(3751),r=a(614);t.default=function(){return l.createElement(u.Z,null,l.createElement(n.Z,{title:"Home"}),l.createElement(r.default,null))}}}]);
+//# sourceMappingURL=component---src-pages-article-js-d0371c419f683f7e4d04.js.map
